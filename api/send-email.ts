b569dc440c75c0b8c4049cdd2d805255dce07389
Seballemandiv <sebastianallemandiv@gmail.com>
2025-09-09 14:23:52 +0200

    Try Resend default testing email address

--- api/send-email.ts
+++ api/send-email.ts
@@ -27,7 +27,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     `;
 
     const result = await resend.emails.send({
-      from: 'Asadazo <sebastianallemandiv@gmail.com>',
+      from: 'Acme <onboarding@resend.dev>',
       to: [TO_EMAIL],
       subject,
       html
