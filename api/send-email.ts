ef76be9814a32b9b8dd47448cf444d8233969d4c
Seballemandiv <sebastianallemandiv@gmail.com>
2025-09-09 14:19:04 +0200

    Fix Resend from address to use Gmail for testing

--- api/send-email.ts
+++ api/send-email.ts
@@ -27,7 +27,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     `;
 
     const result = await resend.emails.send({
-      from: 'Asadazo <onboarding@resend.dev>',
+      from: 'Asadazo <sebastianallemandiv@gmail.com>',
       to: [TO_EMAIL],
       subject,
       html
